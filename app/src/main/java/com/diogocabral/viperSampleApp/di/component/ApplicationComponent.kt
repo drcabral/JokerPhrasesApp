1e56e9509106d9fc408613a856a47612ceac9c00
Diogo Cabral <drkbral@gmail.com>
2019-01-29 00:22:59 -0200

    Using dependency injection for retrofit service.

--- app/src/main/java/com/diogocabral/viperSampleApp/di/component/ApplicationComponent.kt
+++ app/src/main/java/com/diogocabral/viperSampleApp/di/component/ApplicationComponent.kt
@@ -2,9 +2,12 @@ package com.diogocabral.viperSampleApp.di.component
 
 import android.content.Context
 import com.diogocabral.viperSampleApp.di.module.ApplicationModule
+import com.diogocabral.viperSampleApp.di.module.ServiceModule
+import com.diogocabral.viperSampleApp.interactor.service.PhrasesService
 import dagger.Component
 
-@Component(modules = [ApplicationModule::class])
+@Component(modules = [ApplicationModule::class, ServiceModule::class])
 interface ApplicationComponent {
     fun getContext(): Context
+    fun getPhrasesService(): PhrasesService
 }
--- app/src/main/java/com/diogocabral/viperSampleApp/interactor/PhraseInteractor.kt
+++ app/src/main/java/com/diogocabral/viperSampleApp/interactor/PhraseInteractor.kt
@@ -1,16 +1,17 @@
 package com.diogocabral.viperSampleApp.interactor
 
 import com.diogocabral.viperSampleApp.entity.utils.getRandomElement
-import com.diogocabral.viperSampleApp.interactor.utils.HTTPManager
+import com.diogocabral.viperSampleApp.interactor.service.PhrasesService
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 
-class PhraseInteractor @Inject constructor(){
+class PhraseInteractor @Inject constructor(
+        private val phrasesService: PhrasesService){
     fun fetchPhrases(): Observable<String?> {
-        return HTTPManager.phrasesService.fetchRandomPhrase(10)
+        return phrasesService.fetchRandomPhrase(10)
                 .map { it.phrases.getRandomElement()?.joke }
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
